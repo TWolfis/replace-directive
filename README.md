0092395c0b184b55c5096a16656e43cb01f282fc
TWolfis <Thomas.wolfis@gmail.com>
2022-10-28 15:58:03 +0200

    Can now replace directives that equal to given value

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ View and mass replace directives in nagios configuration files
 ## usage 
 
 ```console
-usage: replace-directive.py [-h] [-r, REPLACEMENT] [-f, FILE] [-w] directive
+usage: replace-directive.py [-h] [-r, REPLACEMENT] [-e, EQUALS] [-f, FILE]
+                            [-w]
+                            directive
 
 Find and/or Replace directives in /etc/nagios/generic/contacts.cfg by default
 print values for found directives for possible directives take a look at https
@@ -18,10 +20,12 @@ optional arguments:
   -h, --help            show this help message and exit
   -r, REPLACEMENT, --replacement REPLACEMENT
                         Overwrites directive values with specified replacement
+  -e, EQUALS, --equals EQUALS
+                        Overwrites directive that equals to given value with
+                        specified replacement
   -f, FILE, --file FILE
                         Look for directive in file, default file is
                         /etc/nagios/generic/contacts.cfg
   -w, --write           Overwrite directive with replacement directive
                         replacement
-
 ```
--- replace-directive.py
+++ replace-directive.py
@@ -18,18 +18,30 @@ def GetMatching(configFile,directive):
         print("Not allows to open ",configFile)
         sys.exit()
 
-def ReplaceConfig(configFile,directive,replacementDirective):
+def ReplaceConfig(configFile,directive,replacementDirective,equals=None):
     """Replace specified directive values file with replacement directive value"""
     replacementConfig = ""
 
     try:
         with open(configFile,"r") as file:
             for line in file:
-                #look for lines that contain the given directive that are not comments
-                if re.search(directive,line) and line[0] != "#":
-                    line = line.replace(line,replacementDirective)
-                    replacementConfig = replacementConfig + line + "\n"
+                #look for lines that contain the given directive, trim lines to find comments that are not comments
+                if re.search(directive,line) and line.strip()[0] != "#":
 
+                    #if equals is set to a value and value is found in line
+                    if equals and re.search(equals,line):
+                        line = line.replace(line,replacementDirective)
+                        replacementConfig = replacementConfig + line + "\n"
+                    
+                    #if equals is not set but directive is found in line
+                    elif not equals :
+                        line = line.replace(line,replacementDirective)
+                        replacementConfig = replacementConfig + line + "\n" 
+                    
+                    #if equals is set but not found in line just add line to replacementConfig
+                    else:
+                        replacementConfig = replacementConfig + line
+     
                 #Also add any line that do not match the specified directive to the list since those do not have to be replaced
                 else:
                     replacementConfig = replacementConfig + line
@@ -55,6 +67,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=f"Find and/or Replace directives in {configFile} by default print values for found directives for possible directives take a look at https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/3/en/objectdefinitions.html")
     parser.add_argument("directive",help=f"Print out found matches of directive",type=str)
     parser.add_argument("-r,","--replacement",help="Overwrites directive values with specified replacement", type=str)
+    parser.add_argument("-e,","--equals",help="Overwrites directive that equals to given value with specified replacement", type=str)
     parser.add_argument("-f,","--file",help=f"Look for directive in file, default file is {configFile}", type=str)
     parser.add_argument("-w","--write",help= f"Overwrite directive with replacement directive replacement",action="store_true")
 
@@ -65,20 +78,29 @@ if __name__ == "__main__":
     if args.file:
         configFile = args.file 
 
-    replacement = ""
     if args.replacement:
-        #the spaces are needed to please my autism 
-        replaceDirective = 8*" "+args.directive+27*" "+args.replacement
-        replacementConfig = ReplaceConfig(configFile,directive, replaceDirective)
+        #format replacement directive so that output matches the indentation of the nagios configuration
+        #example of format:
+        #   host_name       example.com
 
+        replaceDirective = "\t"+args.directive+"\t\t\t"+args.replacement
+
+        #if equals is set call replacementConfig with equals
+        if args.equals:
+            print(ReplaceConfig(configFile,directive, replaceDirective, args.equals))
+            print("done")
+            sys.exit()
+        else:
+            replacementConfig = ReplaceConfig(configFile,directive, replaceDirective,None)
+
+        #only write when explicitly specified to prevent unwanted overwrites of configuration files 
         if args.write:
             print(f"Overwritting {configFile}")
-            #only write when explicitly specified to prevent unpleasant surprises, only overwrite after checking your changes 
             WriteNewConfig(configFile,replacementConfig)
         else:
             #replacementConfig could also be redirected to an alternative config file 
             print(replacementConfig)
         sys.exit()
         
-#  print(ReplaceConfig(configFile,replaceDirective))
-    print(GetMatching(configFile,directive=directive))
+    #print(ReplaceConfig(configFile,replaceDirective))
+    print(GetMatching(configFile,directive))
